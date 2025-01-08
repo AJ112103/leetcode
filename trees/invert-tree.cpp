a11421c823caa7c544e3a061e9e74b8c93f705aa
Dhruv Jalan <aamerjalan2@gmail.com>
2025-01-08 23:50:37 +0530

    day4 treees

---
+++ trees/invert-tree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root){
+            return NULL;
+        }
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    };
+};
