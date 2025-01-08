a94097085bef3d618b0afaccf7b6256133564a32
Dhruv Jalan <aamerjalan2@gmail.com>
2025-01-08 23:20:23 +0530

    day4

---
+++ array/remove-duplicates-medium.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int n = 0; n < nums.size(); n++){
+            if(i < 2 || nums[n] != nums[i-2]){
+                nums[i] = nums[n];
+                i++;
+            }
+        }
+        return i;
+    }
+};
