180be735de9cd282beab0c6bf7b86d9de285d499
Dhruv Jalan <aamerjalan2@gmail.com>
2025-01-17 02:44:03 +0530

    day10 twosum sorted

---
+++ array/twosum-sorted.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        while(l < r){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target){
+                return vector<int> {l + 1, r + 1};
+            };
+            if(sum <= target){
+                l++;
+            }else {
+                r--;
+            }
+        }
+        return {};
+    }
+};
